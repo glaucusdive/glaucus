ce9d1ae359d1780ebfd0ce5fcfc399a55ff902a2
madebyporter <general@madebyporter.com>
2025-09-19 14:49:56 -0400

    Adjusted image container height for better layout consistency. Improved visual appeal with updated design elements. Enhanced user experience with clearer shop access.

--- app/components/CardDiveShop.vue
+++ app/components/CardDiveShop.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-6 p-6 border border-neutral-200 rounded-md">
-    <div class="w-full h-48 bg-neutral-100 rounded-sm overflow-hidden">
+    <div class="w-full h-24 xl:h-32  bg-neutral-100 rounded-sm overflow-hidden">
       <img v-if="image" :src="image" :alt="title" class="w-full h-full object-cover" />
     </div>
     <div class="flex flex-col gap-2">
@@ -8,7 +8,9 @@
       <p class="text-sm text-gray-500">{{ location }}</p>
     </div>
     <div class="flex flex-row gap-2">
-      <NuxtLink to="#" class="border border-neutral-300 hover:border-neutral-400 rounded-sm px-4 py-2 text-sm font-bold text-neutral-900">View Shop</NuxtLink>
+      <NuxtLink to="#"
+        class="border border-neutral-300 hover:border-neutral-400 rounded-sm px-4 py-2 text-sm font-bold text-neutral-900">
+        View Shop</NuxtLink>
     </div>
   </div>
 </template>
