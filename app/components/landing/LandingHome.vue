524a16c96c2564f011b2c2ddb03b5185e2c8c463
madebyporter <general@madebyporter.com>
2026-04-14 17:47:58 -0400

    Remove unnecessary whitespace in the hero section of LandingHome.vue for cleaner code.

--- app/components/landing/LandingHome.vue
+++ app/components/landing/LandingHome.vue
@@ -2,7 +2,7 @@
   <main class="bg-[#101214]">
     <LandingHeader />
     <section id="hero" class="bg-[url(/images/landing/glaucus-bg-hero-waves.jpg)] bg-no-repeat bg-top bg-cover lg:min-h-[780px] h-[90dvh]">
-
+      
     </section>
     <section id="whatisglaucus"></section>
     <section id="features"></section>
