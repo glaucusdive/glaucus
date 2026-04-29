8ccb718a3d9b12212589932728d9bba1a631eb31
madebyporter <general@madebyporter.com>
2026-04-29 14:50:38 -0400

    Update DiveShopDetail.vue to reveal depth information in courses list
    - Added a comment to indicate that depth information was previously hidden in the coursesList computed property, enhancing code clarity for future reference.

--- app/components/DiveShopDetail.vue
+++ app/components/DiveShopDetail.vue
@@ -513,6 +513,7 @@ const coursesList = computed(() => {
     .filter(Boolean)
     .map(c => ({
       title: c.certification_name,
+      // depth information was hidden here
       items: [c.description].filter(Boolean).slice(0, 3)
     }))
 })
